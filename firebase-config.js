2c6e86b8e17f4f0f73c7158c4cbbd3f6134af21f
lscpGit <adminsupport@lscpinfo.co.uk>
2025-02-20 16:11:24 +0000

    Create firebase-config.js
    firebase-config-info

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+// Firebase configuration
+    var firebaseConfig = {
+      apiKey: "AIzaSyAEdQwgqpCNkix4Zt2u8NCWn1bg50KZaRA",
+      authDomain: "lscp-database.firebaseapp.com",
+      projectId: "lscp-database",
+      storageBucket: "lscp-database.appspot.com",
+      messagingSenderId: "444036893942",
+      appId: "1:444036893942:web:9a5b5d1fbbcffc6d9af0d7"
+    };
+
+// Export the config
+export default firebaseConfig;
